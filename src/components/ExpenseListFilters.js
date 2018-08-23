35cc6905efc897df62c87ae3901f3cf9a0629bd8
Ben Halverson <benhalverson33@gmail.com>
2018-08-23 14:20:29 -0700

    feat(filter feature): added working filter input text box

---
+++ src/components/ExpenseListFilters.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { setTextFilter } from '../actions/filters';
+const ExpenseListFilters = (props) => (
+  <div><input type="text" value={props.filters.text} onChange={(e) => {
+    e.preventDefault();
+    props.dispatch(setTextFilter(e.target.value));
+  }}/></div>
+)
+const mapStateToProps = (state) => {
+  return {
+    filters: state.filters
+  }
+}
+export default connect(mapStateToProps)(ExpenseListFilters);
