86aa9874bcfcab58e62e868d9feb1bef4067a6aa
Ben Halverson <benhalverson33@gmail.com>
2018-08-29 16:50:32 -0700

    refactor(CreateExpensePage tests): refactored to use beforeEach

--- src/__tests__/components/CreateExpensePage.test.js
+++ src/__tests__/components/CreateExpensePage.test.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { CreateExpensePage } from '../../components/CreateExpensePage';
+const expenses = [
+  {
+    id: '1',
+    description: 'Gum',
+    note: '',
+    amount: 195,
+    createdAt: 0
+  }];
 
-test('should render CreateExpensePage correctly', () => {
-  const onSubmit = jest.fn();
-  const history = { push: jest.fn() };
-  const wrapper = shallow(<CreateExpensePage onSubmit={onSubmit} history={history} />);
+let onSubmit, history, wrapper;
+
+beforeEach(() => {
+  onSubmit = jest.fn();
+  history = { push: jest.fn() };
+  wrapper = shallow(<CreateExpensePage onSubmit={onSubmit} history={history} />);
+});
+
+test('should render AddExpensePage correctly', () => {
   expect(wrapper).toMatchSnapshot();
-})
+});
+
+test('should handle onSubmit', () => {
+  wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
+  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(onSubmit).toHaveBeenLastCalledWith(expenses[1]);
+});
