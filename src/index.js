a85c0e7ee4c3db52aae5912cf7e4bffad3b34fa1
Ben Halverson <benhalverson33@gmail.com>
2018-09-20 15:34:56 -0700

    refactor(service worker): removed service worker file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import { startSetExpenses } from './actions/expenses';
 import configureStore from './store/configureStore';
 const store = configureStore();
