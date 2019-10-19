eb830be7433af070cc2bf1619974ded14b6c9253
Daniel Beigi <dbeigi1995@gmail.com>
2019-10-19 15:07:11 -0400

    Fixed #101 designed login screen

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,35 +1,49 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
+import { Button } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
+
+
 
 const styling = {
-    textAlign: 'center',
-    width: '40%',
-    margin: 'auto'
+  textAlign: 'center',
+  width: '40%',
+  height: '100%',
+  margin: 'auto',
+  paddingTop: '200px'
 }
 
-const styleButton= {
-  marginBottom: '5%',
-  width: '20%',
-  backgroundColor: '#71DDFF'
+const cardStyle = {
+  backgroundColor: '#71DDFF',
+  boxShadow: '10px 10px 5px grey',
+  fontSize: 'large'
+}
+
+const font = {
+  fontSize: '1.90rem',
+  fontWeight: 'normal'
 }
 
 export const LoginPage = ({ startLogin }) => (
-  <div style={styling}><h1 class="mdc-typography--headline1">Budget Application</h1>
-  <div class="mdc-card mdc-elevation--z16" >
-  <div class="mdc-card__primary-action">
-    <div class="mdc-typography mdc-typography--headline6"><p>This is a demo app using firebase and google to login.</p>
-    <p>Once signed in you can add edit and remove expenses.</p>
-    <p>
-      The datepicker is the AirBnB datepicker that is slightly modified to allow
-      for selecting dates in the past.
-    </p></div>
+  <div style={styling}>
+    <Card style={cardStyle}>
+      <Card.Body>
+        <Card.Title class="display-1 font-weight-bold">Budget Application</Card.Title>
+        <hr></hr>
+        <Card.Text class="lead font-weight-lighter" style={font}>
+          <p>This is a demo app using firebase and google to login.</p>
+          <p>Once signed in you can add edit and remove expenses.</p>
+          <p>
+            The datepicker is the AirBnB datepicker that is slightly modified to allow
+            for selecting dates in the past.
+          </p>
+        </Card.Text>
+        <Button as="input" type="button" value="Login" size="lg" onClick={startLogin}/>
+      </Card.Body>
+    </Card>
   </div>
-  <div>
-  <button class="mdc-button mdc-button--raised" style={styleButton} onClick={startLogin}>Login</button>
-  </div>
-</div>
-</div>
+
 );
 
 const mapDispatchToProps = dispatch => ({
