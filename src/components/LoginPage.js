d3d196f7f79e8c4a241accc4c5548a11c535ed7d
Ben Halverson <bhalverson@google.com>
2019-10-21 12:16:47 -0700

    fixed console errors

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,49 +1,52 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { startLogin } from '../actions/auth';
-import { Button } from 'react-bootstrap';
-import { Card } from 'react-bootstrap';
-
-
+import React from "react";
+import { connect } from "react-redux";
+import { startLogin } from "../actions/auth";
+import { Button, Card } from "react-bootstrap";
 
 const styling = {
-  textAlign: 'center',
-  width: '40%',
-  height: '100%',
-  margin: 'auto',
-  paddingTop: '200px'
-}
+  textAlign: "center",
+  width: "40%",
+  height: "100%",
+  margin: "auto",
+  paddingTop: "200px"
+};
 
 const cardStyle = {
-  backgroundColor: '#71DDFF',
-  boxShadow: '10px 10px 5px grey',
-  fontSize: 'large'
-}
+  backgroundColor: "#71DDFF",
+  boxShadow: "10px 10px 5px grey",
+  fontSize: "large"
+};
 
 const font = {
-  fontSize: '1.90rem',
-  fontWeight: 'normal'
-}
+  fontSize: "1.90rem",
+  fontWeight: "normal"
+};
 
 export const LoginPage = ({ startLogin }) => (
   <div style={styling}>
     <Card style={cardStyle}>
       <Card.Body>
-        <Card.Title class="display-1 font-weight-bold">Budget Application</Card.Title>
+        <Card.Title className='display-1 font-weight-bold'>
+          Budget Application
+        </Card.Title>
         <hr></hr>
-        <Card.Text class="lead font-weight-lighter" style={font}>
-          <p>This is a demo app using firebase and google to login.</p>
-          <p>Once signed in you can add edit and remove expenses.</p>
-          <p>
-            The datepicker is the AirBnB datepicker that is slightly modified to allow
-            for selecting dates in the past.
-          </p>
+        <Card.Text className='lead font-weight-lighter' style={font}>
+          This is a demo app using firebase and google to login. <br />
+          Once signed in you can add edit and remove expenses. <br />
+          The datepicker is the AirBnB datepicker that is slightly modified to
+          allow for selecting dates in the past.
         </Card.Text>
-        <Button as="input" type="button" value="Login" size="lg" onClick={startLogin}/>
+        <Button
+          as='input'
+          type='button'
+          value='Login'
+          size='lg'
+          onClick={startLogin}
+          onChange={() => {}}
+        />
       </Card.Body>
     </Card>
   </div>
-
 );
 
 const mapDispatchToProps = dispatch => ({
