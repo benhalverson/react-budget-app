077962abd076ec3f58b89e8dc4e2c118b536e9c3
dib6596 <cory.harkins@cerecore.net>
2019-10-21 20:24:04 -0500

    Replace quotation doubles with singles
    Signed-off-by: dib6596 <cory.harkins@cerecore.net>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { connect } from "react-redux";
-import { startLogout } from "../actions/auth";
-import { Navbar, Nav } from "react-bootstrap";
+import React from 'react';
+import { connect } from 'react-redux';
+import { startLogout } from '../actions/auth';
+import { Navbar, Nav } from 'react-bootstrap';
 
 export const Header = ({ startLogout }) => (
   <>
-    <Navbar bg="primary" variant="dark">
-      <Navbar.Brand href="/dashboard">Budget App</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="/dashboard">Dashboard</Nav.Link>
-        <Nav.Link href="/create">Create Expense</Nav.Link>
-        <Nav.Link href="/" onClick={startLogout}>
+    <Navbar bg='primary' variant='dark'>
+      <Navbar.Brand href='/dashboard'>Budget App</Navbar.Brand>
+      <Nav className='mr-auto'>
+        <Nav.Link href='/dashboard'>Dashboard</Nav.Link>
+        <Nav.Link href='/create'>Create Expense</Nav.Link>
+        <Nav.Link href='/' onClick={startLogout}>
           Logout
         </Nav.Link>
       </Nav>
