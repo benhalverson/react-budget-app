149414bd7ead3c8962c3acd187e08e4efa479009
Brandon Lyons <lyonsbp@umich.edu>
2020-10-27 15:27:13 -0400

    Convert create expense page to hooks

--- src/components/CreateExpensePage.js
+++ src/components/CreateExpensePage.js
@@ -1,27 +1,24 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import ExpenseForm from './ExpenseForm';
-import { startAddExpense } from '../actions/expenses';
+import React from "react";
+import { connect } from "react-redux";
+import ExpenseForm from "./ExpenseForm";
+import { startAddExpense } from "../actions/expenses";
 
-export class CreateExpensePage extends Component {
-  onSubmit = expense => {
-    this.props.startAddExpense(expense);
-    this.props.history.push('/');
+export function CreateExpensePage({ history, startAddExpense }) {
+  const onSubmit = expense => {
+    startAddExpense(expense);
+    history.push("/");
   };
-  render() {
-    return (
-      <div>
-        <h1>Add Expense</h1>
-        <ExpenseForm onSubmit={this.onSubmit} />
-      </div>
-    );
-  }
+
+  return (
+    <div>
+      <h1>Add Expense</h1>
+      <ExpenseForm onSubmit={onSubmit} />
+    </div>
+  );
 }
+
 const mapDispatchToProps = dispatch => ({
   startAddExpense: expense => dispatch(startAddExpense(expense))
 });
 
-export default connect(
-  undefined,
-  mapDispatchToProps
-)(CreateExpensePage);
+export default connect(undefined, mapDispatchToProps)(CreateExpensePage);
